e7e1a07a893fbb124fe4033f0e307c493e4954d6
Joey Bricher <joeybricher@gmail.com>
2015-02-07 11:26:56 -0600

    Removed the Solenoid from the imports.

--- Alice/src/org/usfirst/frc/team4215/robot/Robot.java
+++ Alice/src/org/usfirst/frc/team4215/robot/Robot.java
@@ -7,7 +7,6 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
